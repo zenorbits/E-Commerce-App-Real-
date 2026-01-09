7b687590d2e01d748c18aad738cec5f633df1488
ZenOrbits <zenorbits29@gmail.com>
2026-01-09 17:42:04 +0530

    Sidebar Admin ui done(Basic)

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,18 +1,19 @@
 import { Route, Routes } from 'react-router-dom'
 import GeneralPage from './components/general/GeneralPage'
-import Loginform from './components/AuthPages/Loginform'
 import RegisterForm from './components/AuthPages/RegisterForm'
-import AdminDashboard from './components/admin/AdminDashboard'
+import AdminDashboard from './components/admin/AdminPage'
+import Loginform from './components/AuthPages/LoginForm'
 
 const App = () => {
   return (
     <div className='font-mono h-screen w-screen overflow-x-hidden'>
 
       <Routes>
+        
         <Route path='/' element={<GeneralPage />} />
         <Route path='/login' element={<Loginform />} />
         <Route path='/register' element={<RegisterForm />} />
-        <Route path='/admin/dashboard' element={<AdminDashboard />} />
+        <Route path='/admin/*' element={<AdminDashboard />} />
       </Routes>
     </div>
   )
--- Frontend/src/components/admin/AdminSidebar.jsx
+++ Frontend/src/components/admin/AdminSidebar.jsx
@@ -1,10 +1,81 @@
-import { motion } from "motion/react"
-
+import { motion, stagger } from "motion/react"
+import { FaTv } from "react-icons/fa";
+import { FaUser } from "react-icons/fa";
+import { FaShoppingCart } from "react-icons/fa";
+import { FaBox } from "react-icons/fa";
+import { FaCogs } from "react-icons/fa";
+import { FaUserCircle } from "react-icons/fa";
+import { Link } from "react-router-dom";
 const AdminSidebar = () => {
-    return (
-        <div className="min-h-screen w-screen">
-            <div className="main-side h-screen w-1/6 bg-gradient-to-r from-black via-gray-900 to-black text-white">
 
+    const MotionLink = motion.create(Link);
+
+    const adminFeatures = [
+        {
+            title: 'Dashboard',
+            icon: <FaTv size={22} />,
+            path: "/admin/dashboard"
+        },
+        {
+            title: 'Manage User',
+            icon: <FaUser size={22} />,
+            path: "/admin/users"
+        },
+        {
+            title: 'Manage Orders',
+            icon: <FaShoppingCart size={22} />,
+            path: "/admin/orders"
+        },
+        {
+            title: 'Add Products',
+            icon: <FaBox size={22} />,
+            path: "/admin/products/add"
+        },
+        {
+            title: 'Manage Products',
+            icon: <FaCogs size={22} />,
+            path: "/admin/products/manage"
+        },
+    ];
+
+
+
+
+
+    return (
+        <div className="min-h-screen w-screen ">
+            <div className="main-side hidden sm:flex h-screen w-64 bg-gradient-to-r from-black via-gray-900 to-black text-white px-4 py-8  flex-col justify-between ">
+                <div className="title-list ">
+                    <div className="title text-4xl font-extrabold text-purple-400">
+                        <motion.h1
+                            initial={{
+                                opacity: 0
+                            }}
+                            animate={{
+                                opacity: 1
+                            }}
+                            transition={{
+                                duration: 1.5,
+                            }}
+                        >
+                            ShopOrbits
+                        </motion.h1>
+                    </div>
+                    <div className="list flex pt-16 h-1/2 px-2">
+                        <motion.ul  className="flex flex-col items-start text-xl font-bold gap-6">
+                            {adminFeatures.map((features, idx) => (
+                                <MotionLink  to={features.path} key={idx} className="flex items-center gap-4 hover:text-purple-300 hover:translate-x-1 transition-all">
+                                    {features.icon}
+                                    {features.title}
+                                </MotionLink>
+                            ))}
+                        </motion.ul>
+                    </div>
+                </div>
+                <div className="profile flex gap-6 text-xl font-bold">
+                    <FaUserCircle size={28} />
+                    <div className="h1">Profile</div>
+                </div>
             </div>
         </div>
     )
